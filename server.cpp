4af60681ecc0b89b7ea64f3c4d11bf6fbaf53926
Kacper Gajko <kacper.gajko1@icloud.com>
2025-04-12 12:27:15 +0200

    Working server that i can connect with telnet

---
+++ server.cpp
@@ -0,0 +1,55 @@
+#include <cstdio>
+#include <cstdlib>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+int main() {
+  int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (serverSocket == -1) {
+    printf("Socket failed to start\n");
+    exit(EXIT_FAILURE);
+  } else {
+    printf("Socket started\n");
+  };
+
+  struct sockaddr_in serverAddr, clientAddr;
+  char buffer[1024];
+  int addrLen = sizeof(struct sockaddr_in);
+
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_addr.s_addr = INADDR_ANY;
+  serverAddr.sin_port = htons(8080);
+
+  // Bind
+  if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) ==
+      -1) {
+    printf("Bind failed\n");
+    exit(EXIT_FAILURE);
+  } else {
+    printf("Bind success\n");
+  }
+
+  // Listen
+  if (listen(serverSocket, 3) == -1) {
+    printf("Listen failed\n");
+    exit(EXIT_FAILURE);
+  } else {
+    printf("Listen success\n");
+  }
+
+  // Accept a connection
+  int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr,
+                            (socklen_t *)&addrLen);
+  if (clientSocket == -1) {
+    printf("Accept failed\n");
+    exit(EXIT_FAILURE);
+  } else {
+    printf("Client connected\n");
+  }
+
+  bool running = true;
+  while (running) {
+  }
+  exit(EXIT_SUCCESS);
+}

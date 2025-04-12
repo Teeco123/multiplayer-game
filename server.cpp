2d613f85ea2d7e11312c7c44e2c1cb72a5611860
Kacper Gajko <kacper.gajko1@icloud.com>
2025-04-12 15:56:25 +0200

    added some comment and little cosmetic changes

--- server.cpp
+++ server.cpp
@@ -19,12 +19,15 @@ void HandleClient(int clientSocket, sockaddr_in clientAddr) {
 
   bool running = true;
   while (running) {
+    // Message buffer
     memset(buffer, 0, 1024);
 
+    // Client IP and PORT stringified
     char clientIP[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &(clientAddr.sin_addr), clientIP, INET_ADDRSTRLEN);
     int clientPort = ntohs(clientAddr.sin_port);
+    inet_ntop(AF_INET, &(clientAddr.sin_addr), clientIP, INET_ADDRSTRLEN);
 
+    // Check if server is receiving bytes from client else close connection
     int bytesReceived = recv(clientSocket, buffer, 1024, 0);
     if (bytesReceived <= 0) {
       std::lock_guard lock(consoleMutex);
@@ -33,21 +36,26 @@ void HandleClient(int clientSocket, sockaddr_in clientAddr) {
       break;
     }
 
+    // Print client message
     {
       std::lock_guard lock(consoleMutex);
       printf("%s:%d - %s", clientIP, clientPort, buffer);
     }
   }
+
+  // Cleanup client socket
   close(clientSocket);
 }
 
 int main() {
   std::vector<std::thread> threads;
 
+  // Address structs and length
   int serverSocket, clientSocket;
   struct sockaddr_in serverAddr, clientAddr;
   int addrLen = sizeof(struct sockaddr_in);
 
+  // Create server socket
   serverSocket = socket(AF_INET, SOCK_STREAM, 0);
   if (serverSocket == -1) {
     printf("Socket failed to start\n");
@@ -56,33 +64,37 @@ int main() {
     printf("Socket started\n");
   };
 
+  // Set reuse addres for socket
   int opt = 1;
   if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) <
       0) {
-    printf("setsockopt failed: %s", strerror(errno));
+    printf("Setsockopt failed: %s", strerror(errno));
     close(serverSocket);
     exit(EXIT_FAILURE);
   }
 
+  // Set addres family and port
   serverAddr.sin_family = AF_INET;
   serverAddr.sin_addr.s_addr = INADDR_ANY;
   serverAddr.sin_port = htons(PORT);
 
+  // Bind server socket
   if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) ==
       -1) {
     printf("Bind failed\n");
     close(serverSocket);
     exit(EXIT_FAILURE);
   } else {
-    printf("Bind success\n");
+    printf("Binded successfully\n");
   }
 
+  // Start listening
   if (listen(serverSocket, 3) == -1) {
     printf("Listen failed\n");
     close(serverSocket);
     exit(EXIT_FAILURE);
   } else {
-    printf("Listen success\n");
+    printf("Listening successfully\n");
   }
 
   printf("-------------------------------------\n");
@@ -90,14 +102,18 @@ int main() {
   printf("-------------------------------------\n");
 
   while (true) {
+    // Create client socket
     clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr,
                           (socklen_t *)&addrLen);
+
+    // Accept new connection from client
     if (clientSocket == -1) {
       printf("Accept failed %s\n", strerror(errno));
     } else {
       printf("New client is connecting\n");
     }
 
+    // Get ip and port of client
     char clientIP[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &(clientAddr.sin_addr), clientIP, INET_ADDRSTRLEN);
     int clientPort = ntohs(clientAddr.sin_port);
@@ -107,6 +123,7 @@ int main() {
       printf("Client connected - %s:%d \n", clientIP, clientPort);
     }
 
+    // Create new thread for a client and detach it
     threads.push_back(std::thread(HandleClient, clientSocket, clientAddr));
     threads.back().detach();
   }

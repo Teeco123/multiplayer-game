d1e9e8822e960475463301809eeaa40bbae706b3
Kacper Gajko <kacper.gajko1@icloud.com>
2025-04-12 14:27:38 +0200

    multiple clients (threading)

--- server.cpp
+++ server.cpp
@@ -1,53 +1,19 @@
+#include <arpa/inet.h>
+#include <cerrno>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <mutex>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <thread>
+#include <unistd.h>
+#include <vector>
 
-int main() {
-  int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+std::mutex consoleMutex;
 
-  if (serverSocket == -1) {
-    printf("Socket failed to start\n");
-    exit(EXIT_FAILURE);
-  } else {
-    printf("Socket started\n");
-  };
-
-  struct sockaddr_in serverAddr, clientAddr;
+void HandleClient(int clientSocket) {
   char buffer[1024];
-  int addrLen = sizeof(struct sockaddr_in);
-
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_addr.s_addr = INADDR_ANY;
-  serverAddr.sin_port = htons(8080);
-
-  // Bind
-  if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) ==
-      -1) {
-    printf("Bind failed\n");
-    exit(EXIT_FAILURE);
-  } else {
-    printf("Bind success\n");
-  }
-
-  // Listen
-  if (listen(serverSocket, 3) == -1) {
-    printf("Listen failed\n");
-    exit(EXIT_FAILURE);
-  } else {
-    printf("Listen success\n");
-  }
-
-  // Accept a connection
-  int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr,
-                            (socklen_t *)&addrLen);
-  if (clientSocket == -1) {
-    printf("Accept failed\n");
-    exit(EXIT_FAILURE);
-  } else {
-    printf("Client connected\n");
-  }
 
   bool running = true;
   while (running) {
@@ -56,11 +22,82 @@ int main() {
     int bytesReceived = recv(clientSocket, buffer, 1024, 0);
 
     if (bytesReceived <= 0) {
+      std::lock_guard lock(consoleMutex);
       printf("Client disconnected\n");
       break;
     }
 
-    printf("Received: %s", buffer);
+    {
+      std::lock_guard lock(consoleMutex);
+      printf("Received: %s", buffer);
+    }
+  }
+  close(clientSocket);
+}
+
+int main() {
+  std::vector<std::thread> threads;
+
+  int serverSocket, clientSocket;
+  struct sockaddr_in serverAddr, clientAddr;
+  int addrLen = sizeof(struct sockaddr_in);
+
+  serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+  if (serverSocket == -1) {
+    printf("Socket failed to start\n");
+    exit(EXIT_FAILURE);
+  } else {
+    printf("Socket started\n");
+  };
+
+  int opt = 1;
+  if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) <
+      0) {
+    printf("setsockopt failed: %s", strerror(errno));
+    close(serverSocket);
+    exit(EXIT_FAILURE);
+  }
+
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_addr.s_addr = INADDR_ANY;
+  serverAddr.sin_port = htons(8080);
+
+  if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) ==
+      -1) {
+    printf("Bind failed\n");
+    close(serverSocket);
+    exit(EXIT_FAILURE);
+  } else {
+    printf("Bind success\n");
+  }
+
+  if (listen(serverSocket, 3) == -1) {
+    printf("Listen failed\n");
+    close(serverSocket);
+    exit(EXIT_FAILURE);
+  } else {
+    printf("Listen success\n");
+  }
+
+  while (true) {
+    clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr,
+                          (socklen_t *)&addrLen);
+    if (clientSocket == -1) {
+      printf("Accept failed %s\n", strerror(errno));
+    } else {
+      printf("New client is connecting\n");
+    }
+
+    char clientIP[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &(clientAddr.sin_addr), clientIP, INET_ADDRSTRLEN);
+
+    {
+      std::lock_guard lock(consoleMutex);
+      printf("Client connected - %s \n", clientIP);
+    }
+
+    threads.push_back(std::thread(HandleClient, clientSocket));
+    threads.back().detach();
   }
 
   exit(EXIT_SUCCESS);
